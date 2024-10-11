a97a081f3d0eec432893e37c35d1bcf27797b0ca
Muhamad Eko Alfianto <eko.alfian14@gmail.com>
2024-10-11 16:22:50 +0700

    add notification and events

--- resources/views/livewire/chirps/list.blade.php
+++ resources/views/livewire/chirps/list.blade.php
@@ -25,6 +25,13 @@ $edit = function (Chirp $chirp) {
     $this->getChirps();
 };  
 
+$delete = function (Chirp $chirp) {
+    $this->authorize('delete', $chirp);
+ 
+    $chirp->delete();
+ 
+    $this->getChirps();
+}; 
 ?>
 
 <div>
@@ -57,6 +64,9 @@ $edit = function (Chirp $chirp) {
                                 <x-dropdown-link wire:click="edit({{ $chirp->id }})">
                                     {{ __('Edit') }}
                                 </x-dropdown-link>
+                                <x-dropdown-link wire:click="delete({{ $chirp->id }})" wire:confirm="Are you sure to delete this chirp?"> 
+                                    {{ __('Delete') }}
+                                </x-dropdown-link> 
                             </x-slot>
                         </x-dropdown>
                     @endif
