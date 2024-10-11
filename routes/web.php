f35a56bd91788e5255fcd433b0736c1348dc8da5
Muhamad Eko Alfianto <eko.alfian14@gmail.com>
2024-10-11 15:27:31 +0700

    add function create chirps

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ChirpController;
 
 Route::view('/', 'welcome');
+
+Route::get('chirps', [ChirpController::class, 'index'])
+    ->middleware(['auth', 'verified'])
+    ->name('chirps'); 
 
 Route::view('dashboard', 'dashboard')
     ->middleware(['auth', 'verified'])
